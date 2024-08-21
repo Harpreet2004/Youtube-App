04503408d8034d6d703e4f2b96b48f19d2e6d860
harpreet2004 <harpreetsingh5066@gmail.com>
2024-08-21 12:42:39 +0530

    writing controllers for user registration

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,31 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import {apiError} from "../utils/apiErrors.js"
+import {User} from "../models/user.models.js"
 
 const registerUser = asyncHandler(async (req,res) => {
-    res.status(200).json({
-        message: "OK"
+    // get user details from frontend
+    // get if any field is empty or not
+    // check if user already register: username,email
+    // check for images, avatars
+    // upload them to cloudinary
+    // check if successfully uploaded on cloudinary
+    // create user object - create entry in db
+    // remove password and refresh token from response
+    // check for user creation
+    // return res
+    
+    const {userName,email,fullName,password} = req.body;
+    
+    if([userName,email,fullName,password].some((field) => field?.trim() === ""))
+        throw new apiError(400,"All fields are required");
     })
-})
+
+    const userExists = User.findOne({
+        $or: [{userName},{email}]
+    })
+
+    if(userExists) {
+        throw new apiError(409,"User with email or username already exists.")
+    }
 
 export {registerUser};
