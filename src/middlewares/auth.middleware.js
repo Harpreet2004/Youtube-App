193cfe23a2b48d43060c739f379e98febf72a63a
harpreet2004 <harpreetsingh5066@gmail.com>
2024-08-29 08:14:27 +0530

    routes & logout func added

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -3,21 +3,25 @@ import { ApiError } from "../utils/apiErrors";
 import { asyncHandler } from "../utils/asyncHandler";
 import jwt from "jsonwebtoken";
 
-export const verifyJwt = asyncHandler(async (req,res,next) => {
-    const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ","");
-
-    if(!token) {
-        throw new ApiError(409,"Unauthorized request");
+export const verifyJwt = asyncHandler(async (req,_,next) => {
+    try {
+        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ","");
+    
+        if(!token) {
+            throw new ApiError(409,"Unauthorized request");
+        }
+    
+        const decodedToken = jwt.verify(token,process.env.ACCESS_TOKEN_SECRET);
+    
+        const user = await User.findById(decodedToken?._id).select("-password -refreshToken");
+    
+        if(!user) {
+            throw new ApiError(401,"Invalid Access Token");
+        }
+    
+        req.user = user;
+        next();
+    } catch (error) {
+        throw new ApiError(401,error?.message || "invalid access token");
     }
-
-    const decodedToken = jwt.verify(token,process.env.ACCESS_TOKEN_SECRET);
-
-    const user = await User.findById(decodedToken?._id).select("-password -refreshToken");
-
-    if(!user) {
-        throw new ApiError(401,"Invalid Access Token");
-    }
-
-    req.user = user;
-    next();
 })
