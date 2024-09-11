ee847e53de5e30c509f5fe7eddc4ebfae589004e
harpreet2004 <harpreetsingh5066@gmail.com>
2024-09-11 17:24:53 +0530

    updating tweet controller

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -1,6 +1,5 @@
 import mongoose, { isValidObjectId } from "mongoose"
 import {Tweet} from "../models/tweet.models.js"
-import {User} from "../models/user.models.js"
 import {ApiError} from "../utils/apiErrors.js"
 import { ApiResponse } from "../utils/apiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
@@ -13,39 +12,39 @@ const createTweet = asyncHandler(async (req, res) => {
         throw new ApiError(400,"Content is required");
     }
  
-    const tweet = await Tweet.aggregate([
-        {
-            $match: {
-                _id: mongoose.Types.ObjectId
-            },
-        },
-        {
-            $lookup: {
-                from: "users",
-                localField: "owner",
-                foreignField: "_id",
-                as: "owner"
-            },
-            pipeline: [
-                {
-                    $project: {
-                        userName:1,
-                        fullName:1,
-                        avatar:1
-                    }
-                },
-                {
-                    $addFields: {
-                        $first: "$owner"
-                    }
-                }
-            ]
-        }
-])
+//     const tweet = await Tweet.aggregate([
+//         {
+//             $match: {
+//                 _id: mongoose.Types.ObjectId
+//             },
+//         },
+//         {
+//             $lookup: {
+//                 from: "users",
+//                 localField: "owner",
+//                 foreignField: "_id",
+//                 as: "owner"
+//             },
+//             pipeline: [
+//                 {
+//                     $project: {
+//                         userName:1,
+//                         fullName:1,
+//                         avatar:1
+//                     }
+//                 },
+//                 {
+//                     $addFields: {
+//                         $first: "$owner"
+//                     }
+//                 }
+//             ]
+//         }
+// ])
 
     const createTweet = await Tweet.create({
         content,
-        owner
+        owner: req.user?._id
     })
 
     if(!createTweet) {
@@ -57,7 +56,7 @@ const createTweet = asyncHandler(async (req, res) => {
     .json(
         new ApiResponse(
             200,
-            tweet[0],
+            createTweet,
             "Tweet was created successfully"
         )
     )
@@ -66,43 +65,91 @@ const createTweet = asyncHandler(async (req, res) => {
 
 const getUserTweets = asyncHandler(async (req, res) => {
     // TODO: get user tweets
-    const {userid} = req.params;
+    const {userId} = req.params;
 
     //maybe trim required for the userid 
     //if bug happens try that too
-    if(!userid) {
+    if(!isValidObjectId(userId)) {
         throw new ApiError(400,"Could not get the userid")
     }
 
-    // const getTweets = await Tweet.aggregate([
-    //     {
-    //         $match: {
-    //             _id: userid
-    //         }
-    //     },
-    //     {
-    //         $lookup: {
-    //             from: ""
-    //         }
-    //     }
-    // ])
+    const findAllUserTweets = await Tweet.find({
+        owner: new mongoose.Types.ObjectId(userId)
+    });
 
-    const findUser = await Tweet.findById(userid);
-
-    if(!findUser) {
-        throw new ApiError(400,"Not able to find the user with the id");
+    if(!findAllUserTweets.length === 0) {
+        throw new ApiError(400,"Not able to find the tweets");
     }
 
-    
+    return res
+    .status(200)
+    .json(
+        200,
+        findAllUserTweets,
+        "All tweets fetched successfully"
+    )
 
 })
 
 const updateTweet = asyncHandler(async (req, res) => {
     //TODO: update tweet
+    const {tweetId} = req.params;
+    const {content} = req.body;
+
+    if(!isValidObjectId(tweetId)) {
+        throw new ApiError(400,"Required tweet could not be found");
+    }
+
+    if(!content) {
+        throw new ApiError(400,"Tweet content is required");
+    }
+
+
+    const tweet = await Tweet.findByIdAndUpdate(
+        tweetId,
+        {
+            $set:{ 
+                content: content
+            }
+        },
+        {new: true}
+    ).select("-owner")
+
+    if(!tweet){
+        throw new ApiError(500,"There was some error while updating the tweet")
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            tweet,
+            "Tweet is updated successfully"
+        )
+    )
 })
 
 const deleteTweet = asyncHandler(async (req, res) => {
     //TODO: delete tweet
+    const {tweetId} = req.params._id;
+
+    if(!isValidObjectId(tweetId)) {
+        throw new ApiError(400,"The tweet you want to delete cannot be accessed.")
+    }
+
+    const tweetDelete = await Tweet.findByIdAndDelete(tweetId);
+
+    if(!tweetDelete) {
+        throw new ApiError(500,"Something went wrong while deleting the tweet.")
+    }
+
+    return res
+    .status(
+        200,
+        tweetDelete,
+        "Tweet is deleted successfully"
+    )
 })
 
 export {
