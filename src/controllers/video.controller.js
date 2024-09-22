a61f564b69274d1694701cdbcb027cc1304b93da
harpreet2004 <harpreetsingh5066@gmail.com>
2024-09-22 13:09:05 +0530

    completing the video controller

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -1,6 +1,5 @@
 import mongoose, {isValidObjectId} from "mongoose"
 import {Video} from "../models/video.models.js"
-import {User} from "../models/user.models.js"
 import {ApiError} from "../utils/apiErrors.js"
 import {ApiResponse} from "../utils/apiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
@@ -8,13 +7,126 @@ import {uploadOnCloudinary, deleteFromCloudinary} from "../utils/cloudinary.js"
 
 
 const getAllVideos = asyncHandler(async (req, res) => {
-    const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query
-    //TODO: get all videos based on query, sort, pagination
-})
+    const { page = 1, limit = 10, query = "", sortBy = "createdAt", sortType = 1, userId = ""} = req.query
+
+    var video;
+    try {
+        video = await Video.aggregate([
+            {
+                $match:{
+                    $or:[
+                        { title: { $regex: query, $options: "i"} }, //i refers to case insensitive
+                        { description: { $regex: query, $options: "i"} }
+                    ]
+                }
+            },
+            {
+                $lookup:{
+                    from: "users",
+                    localField: "owner",
+                    foreignField: "_id",
+                    as: "owner",
+                    pipeline: [
+                        {
+                            $project:{
+                                userName:1,
+                                avatar:"$avatar.url",
+                                fullName:1
+                            }
+                        }
+                    ]
+                }
+            },
+            {
+                $addFields:{
+                    owner:{
+                        $first: "$owner"
+                    },
+                }
+            },
+            {
+                $sort:{
+                    [sortBy || "createdAt"]:sortType || 1
+                }
+            }
+        ])
+    } catch (error) {
+        throw new ApiError(500,error.message || "Internal server error while aggregating video");
+    }
+
+
+    const options = {
+        page,
+        limit,
+        customLabels: {
+            totalDocs : "totalVideos",
+            docs: "videos"
+        },
+        skip: (page - 1) * limit,
+        limit: parseInt(limit)
+    }
+
+    Video.aggregatePaginate(video,options)
+        .then(result => {
+            if(result?.videos?.length === 0) {
+                return res.status(200).json(new ApiResponse(200,[],"No vidoes found!"))
+            }
+
+            return res.status(200)
+                .json(
+                    new ApiResponse(
+                        200,
+                        [result],
+                        "Video fetched successfully"
+                    )
+                )
+        }).catch(error => {
+            throw new ApiError(500, error?.message || "INTERNAL SERVER ERROR!! Something went wrong while fetching the videos");
+        })
+})  
 
 const publishAVideo = asyncHandler(async (req, res) => {
-    const { title, description} = req.body
-    // TODO: get video, upload to cloudinary, create video
+    const { title, description} = req.body;
+    
+    if(!title && !description) {
+        throw new ApiError(504,"Title and descriptions are required!");
+    }
+
+    const videoLocalPath = req.files?.videoFile[0].path;
+
+    const thumbnailLocalPath = req.files?.thumbnail[0].path;
+
+    if(!videoLocalPath && !thumbnailLocalPath) {
+        throw new ApiError(500,"Local path is not correct for video & thumbnail");
+    }
+
+    const video = await uploadOnCloudinary(videoLocalPath);
+    
+    const thumbnail = await uploadOnCloudinary(thumbnailLocalPath);
+
+    const videoPublished = await Video.create({
+        title,
+        description,
+        videoFile: video?.url,
+        thumbnail: thumbnail?.url,
+        isPublished: true,
+        duration: video?.duration,
+        owner: req.user?._id
+    })
+
+    if(!videoPublished){
+        throw new ApiError(503,"Something went wrong while publishing the vidoe!!");
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            videoPublished,
+            "Video Published Successfully!"
+        )
+    );
 })
 
 const getVideoById = asyncHandler(async (req, res) => {
@@ -149,6 +261,32 @@ const deleteVideo = asyncHandler(async (req, res) => {
 
 const togglePublishStatus = asyncHandler(async (req, res) => {
     const { videoId } = req.params
+
+    if(!isValidObjectId(videoId)){
+        throw new ApiError(420,"Video does not exists");
+    }
+
+    const findVideo = await Video.findOne({
+        _id: videoId,
+        Owner: req.user?._id
+    });
+
+    if(!findVideo){
+        throw new ApiError(540,"Something went wrong while finding the video!");
+    }
+
+    findVideo.isPublished = !findVideo.isPublished
+    await findVideo.save();
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            findVideo.isPublished,
+            "isPublished toggled Successfully!"
+        )
+    );
 })
 
 export {
